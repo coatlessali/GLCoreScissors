b35442daa6cdf75ba6a9e142faf4aa2078b06623
coatlessali <coatlessali@protonmail.com>
2026-02-14 22:36:41 -0600

    update to work with modern ultrakill

--- Plugin.cs
+++ Plugin.cs
@@ -16,61 +16,23 @@ namespace GLCoreScissors
 		public static Plugin Instance;
 		private void Awake()
 		{
-			// MacOS is special once again
-			string funny = (Application.platform == RuntimePlatform.OSXPlayer) ? "../.." : "..";
-			// Name of original Prefs file
-			string fileName = Path.Combine(Application.dataPath, funny, "Preferences", "Prefs.json");
-			// Name of new copy
-			string refCopy = Path.Combine(Application.dataPath, funny, "Preferences", "GLCoreScissors.json");
-
-			// Make Copy to avoid shared access error
-			try {
-				File.Copy(fileName, refCopy, true);
-			}
-			catch (IOException iox) {
-				Logger.LogError(iox.Message);
-			}
-
+			
 			// Load Plugin
 			Instance = this;
-			Logger.LogInfo("Loading Plugin... Restart required after settings toggle.");
+			Logger.LogInfo("This will force \"isGLCore\" to be false. This may have unintended consequences. You have been warned.");
 			Harmony har = new Harmony("com.coatlessali.glcorescissors");
 			har.PatchAll();
 		}
 	}
 
-	// Patching the GLCoreBandaid function
-	[HarmonyPatch(typeof(GLCoreBandaid), nameof(GLCoreBandaid.OnEnable))]
-	class GLCoreScissors : MonoBehaviour
-	{	
-		static void Postfix(ref GameObject ___optionsToHide, ref GameObject ___dialogToShow)
-		{
-			// Reenable toggles
-			___optionsToHide.SetActive(true);
-			___dialogToShow.SetActive(false);
-		}
-	}
-
 	// Patching PostProcessV2_Handler
 	[HarmonyPatch(typeof(PostProcessV2_Handler), nameof(PostProcessV2_Handler.Start))]
-	class EnableOutlines : MonoSingleton<PostProcessV2_Handler>
+	class Start : MonoSingleton<PostProcessV2_Handler>
 	{
-		static void Postfix()
-		{
-			// once again checking for MacOS and handling it accordingly
-			string funny = (Application.platform == RuntimePlatform.OSXPlayer) ? "../.." : "..";
-			// get the copy of the .json file we made earlier
-			string fileName = Path.Combine(Application.dataPath, funny, "Preferences", "GLCoreScissors.json");
-			// reading all of the text...? uh, okay moving on
-			string options = File.ReadAllText(fileName);
-
-			// why
-			bool optionsCheck = options.Contains("\"simplifyEnemies\": true,");
-
-			// check for option and enable, still requires a restart but at least it can be toggled in-game now
-			if (optionsCheck == true) {
-				Shader.DisableKeyword("NOOUTLINES");
-			}
+		static void Postfix(ref bool ___isGLCore)
+		{	
+		    ___isGLCore = false;
 		}
 	}
 }
+
